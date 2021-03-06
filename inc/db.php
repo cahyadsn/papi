a08ce242b49b77ae1d0dd1313e0262fd40bba6fc
Cahya DSN <cahyadsn@gmail.com>
2021-03-06 22:56:56 +0700

    Update db.php
    menambahkan pernyataan License ke MIT License

--- inc/db.php
+++ inc/db.php
@@ -1,4 +1,30 @@
 <?php
+/*
+================================================================================
+ *  BISMILLAAHIRRAHMAANIRRAHIIM - In the Name of Allah, Most Gracious, Most Merciful
+================================================================================
+FILENAME     : inc/db.php
+AUTHOR       : CAHYA DSN
+CREATED DATE : 2017-04-09
+UPDATED DATE : 2021-03-06
+DEMO SITE    : http://psycho.cahyadsn.com/papi
+SOURCE CODE  : https://github.com/cahyadsn/papi
+================================================================================
+This program is free software; you can redistribute it and/or modify it under the
+terms of the MIT License.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+See the MIT License for more details
+
+copyright (c) 2017-2021 by cahya dsn; cahyadsn@gmail.com
+================================================================================ */  
 $dbhost='localhost';
 $dbuser='root';
 $dbpass='';
